ff2efe18ee02e4c1a890874d91fc81cfea87e542
PaulienTensen <31660120+PaulienTensen@users.noreply.github.com>
2017-12-11 10:51:05 +0100

    'functie'

--- RailNL/Code/functies/minuten.py
+++ RailNL/Code/functies/minuten.py
@@ -1,7 +1,20 @@
-def minuten(alle_tijdsduur):
+## 
+#Namen: Thomas Van Doren, Mattia Caso, Paulien Tensen. 
+#Vak: Heuristieken. 
+#Case: Rail NL. 
+#
+#In dit bestand staat de functie voor het opsommen van aantal minuten. 
+#
+##
 
+def minuten(alle_tijdsduur):
+    
+    # Zet totale tijdsduur op 0. 
     totale_tijdsduur = 0
+    
+    # Tel de tijdsduur van iedere verbinding op bij totale tijdsduur. 
     for i in range(len(alle_tijdsduur)):
         totale_tijdsduur += alle_tijdsduur[i]
-        
+    
+    # Return de totale tijdsduur. 
     return totale_tijdsduur
--- RailNL/Code/functies/scorefunctie.py
+++ RailNL/Code/functies/scorefunctie.py
@@ -1,16 +1,35 @@
-def score(alle_trajecten, totale_tijdsduur, sporen, totaal_sporen):
+## 
+#Namen: Thomas Van Doren, Mattia Caso, Paulien Tensen. 
+#Vak: Heuristieken. 
+#Case: Rail NL. 
+#
+#In dit bestand staat de functie om de score functie mee te berekenen. 
+#
+#
+##
 
+def score(alle_trajecten, totale_tijdsduur, sporen, totaal_sporen):
+    
+    # Pak allen trajecten.
     s = len(alle_trajecten)
     t = s
     
+    # Als de lengte van alle trajecten 1 is, dus trein is nergens heen gegaan. 
+    # Tel dit trjaect niet mee. 
     for i in range(s):
         if len(alle_trajecten[i]) == 1:
             t = t-1
     
+    # Haal gegevens op van sporen en totale tijdsduur.  
     gebruikte_sporen = len(sporen)
     min = totale_tijdsduur
+    
+    # Percentage = gebruikte sporen delen door het totaal mogelijke aantal sporen.
     p = gebruikte_sporen / totaal_sporen
     
+    # Score functie waarbij p) percentage bereden sporen. t) aantal treinen
+    # min) totale tijdsduur. 
     score = p*10000 - (t*20 + min/10000) 
     
+    # Return de score. 
     return score
