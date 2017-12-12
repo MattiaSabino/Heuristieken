c6478db039bea269924d204b6632ada3fc382fb9
PaulienTensen <31660120+PaulienTensen@users.noreply.github.com>
2017-12-12 12:48:27 +0100

    'string'

--- RailNL/Code/functies/minuten.py
+++ RailNL/Code/functies/minuten.py
@@ -1,20 +1,17 @@
-## 
-#Namen: Thomas Van Doren, Mattia Caso, Paulien Tensen. 
-#Vak: Heuristieken. 
-#Case: Rail NL. 
+# Course: Huristieken
+# Namen: Thomas Van Doren, Mattia Caso, Paulien Tensen. 
+# Case: Rail NL
 #
-#In dit bestand staat de functie voor het opsommen van aantal minuten. 
+#Berekenen van totaal aantal minuten van de lijnvoering.
 #
-##
 
 def minuten(alle_tijdsduur):
+    """Deze functie returned de totale tijdsduur van de lijnvoering."""
     
-    # Zet totale tijdsduur op 0. 
     totale_tijdsduur = 0
     
     # Tel de tijdsduur van iedere verbinding op bij totale tijdsduur. 
     for i in range(len(alle_tijdsduur)):
         totale_tijdsduur += alle_tijdsduur[i]
     
-    # Return de totale tijdsduur. 
     return totale_tijdsduur
